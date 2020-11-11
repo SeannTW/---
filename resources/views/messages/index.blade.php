24c4bdcf2866c4569adb807ee5ad2de42d9deeba
SeannTW <oopp150033@gmail.com>
2020-11-11 23:25:20 +0800

    修改:畫面調整

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -3,10 +3,10 @@
 @section('title', 'Message Board')
 
 @section('content')
-<div class="container">
-  <div class="justify-content-center" style="width:500px;">
-    <div class="card my-5">
-      <h5 class="card-header">MessageBoard:</h5>
+<div class="container" style="width:500px;">
+  <div class="justify-content-center">
+    <div class="card my-5" style="background-color:#c2c2c2;border:3px #7e7e7e solid;border-radius:18px;">
+      <h5 class="card-header" >MessageBoard:</h5>
       <div class="card-body">
         <form action="{{ route('messages.store') }}" enctype="multipart/form-data" method="POST">
           @csrf
@@ -32,24 +32,26 @@
           <tr>
             <div class="media mb-4">
               <img class="d-flex mr-3 rounded-circle" src="http://localhost/Larave%20Project/Project-MessageBoard/public/uploads/avatars/{{ $message->avatar }}" width="60"/>
-                <div class="media-body">
-                  <font size="1">
-                    @if ($message->updated_at)
-                      <a>( 已編輯 )</a>
-                    @endif
-                  </font>
-                  <div>
-                    <a href="messages/{{ $message->id }}/edit" class="btn btn-default btn-sm" style="float:right;">編輯留言</a>
-                    <h2 class="mt-0" style="word-break: break-all;">{{ $message->name }}</h2>
+                <div class="media-body" style="border-radius:18px;background-color:#c9b6b6;">
+                  <div style="margin:10px 20px -5px 20px">
+                    <font size="1">
+                      @if ($message->updated_at)
+                        <a>( 已編輯 )</a>
+                      @endif
+                    </font>
+                    <div>
+                      <a href="messages/{{ $message->id }}/edit" class="btn btn-default btn-sm" style="float:right;">編輯留言</a>
+                      <h2 class="mt-0" style="word-break: break-all;">{{ $message->name }}</h2>
+                    </div>
+                      <h7>{{ $message->created_at }}</h7>
+                    </br>
+                    </br>
+                    <div>
+                      <a href="replies/{{ $message->id }}" class="btn btn-info btn-sm" style="float:right;">回覆</a>
+                    </div>
+                    <p style="word-break: break-all;">{{ $message->content }}</p>
+                    <br/>
                   </div>
-                    <h7>{{ $message->created_at }}</h7>
-                  </br>
-                  </br>
-                  <div>
-                    <a href="replies/{{ $message->id }}" class="btn btn-info btn-sm" style="float:right;">回覆</a>
-                  </div>
-                  <p style="word-break: break-all;">{{ $message->content }}</p>
-                  <br/>
                 </div>
             </div>
           </tr>
